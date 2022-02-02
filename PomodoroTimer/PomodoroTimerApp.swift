0106ce10648f5bf20b54d80c8781b831452e77f0
TorgeirEIkeland <69902668+TorgeirEIkeland@users.noreply.github.com>
2022-02-02 09:35:12 +0100

    Created project

---
+++ PomodoroTimer/PomodoroTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroTimerApp.swift
+//  PomodoroTimer
+//
+//  Created by Torgeir Eikeland on 02/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
