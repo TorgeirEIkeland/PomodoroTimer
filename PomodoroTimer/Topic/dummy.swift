0731e16040eb7e4603e034269e309394c3017190
joeych8 <73697534+joeych8@users.noreply.github.com>
2022-02-07 13:43:01 +0100

    dummyFile

---
+++ PomodoroTimer/Topic/dummy.swift
@@ -0,0 +1,20 @@
+//
+//  dummy.swift
+//  PomodoroTimer
+//
+//  Created by Joey Chau on 07/02/2022.
+//
+
+import SwiftUI
+
+struct dummy: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct dummy_Previews: PreviewProvider {
+    static var previews: some View {
+        dummy()
+    }
+}
