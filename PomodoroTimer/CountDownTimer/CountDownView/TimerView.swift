db2e87899ddfb4cb823d40661f1d4c2ab8d499cb
joeych8 <73697534+joeych8@users.noreply.github.com>
2022-02-10 18:05:11 +0100

    timerAlertBox

--- PomodoroTimer/CountDownTimer/CountDownView/TimerView.swift
+++ PomodoroTimer/CountDownTimer/CountDownView/TimerView.swift
@@ -7,133 +7,134 @@
 import SwiftUI
 
 struct TimerView: View {
+    
     @StateObject var viewModel = TimerViewModel()
-    @State private var timeRemaining = 3
-    let timer = Timer.publish(every: 1, on: .main, in: .common) //.autoconnect //replace autoconnect.
-    
-    /* timer pause when app in background ?
-     @Environment(\.scenePhase) var scenePhase
-     @State private var isActive = true
-     */
-    
-    //var counter = 0
+    // @State private var showAlert = false
+    @State var breakTime = false
     
     var body: some View {
+        
+        
+        VStack{
+        
             VStack{
                 
                 VStack{
                     
                     Spacer()
                     
-                    if timeRemaining >= 1 {
+                    if breakTime == false {
                         Image(Consts.Images.redTomato)
                             .resizable()
                             .frame(width: 260, height: 260 )
                             .onTapGesture {
-                                timer.connect() //Works, but??? -->
+                                viewModel.timer.connect()
+                            }
+                            .alert("It´s time for a break", isPresented: $viewModel.showAlert) {
+                                Button("OK", role: .cancel) { breakTime = true }
                             }
                         
-                        
                     } else {
+                        
+                        
                         
                         Image(Consts.Images.greenTomato)
                             .resizable()
                             .frame(width: 260, height: 290 )
                             .blur(radius: 5)
                             .overlay(
-                                Text("Quote:\(viewModel.quotes[0].q)\nAuthor:\(viewModel.quotes[0].a)")
+                                Text("\(viewModel.quotes[0].q)\n\n\(viewModel.quotes[0].a)")
                                     .font(.system(size: 18))
-                                
-                                   
                             )
-                        
-                    }
-                    
-                    if timeRemaining >= 1 {
-                        Text("\(timeRemaining)")
-                            .font(.system(size: 32))
-                        
-                        
-                        
-                    } else {
-                        
-                        Text("Pause timer")
-                            .font(.system(size: 32))
-                        
-                        Text("MM:SS") // pause timer logic
-                            .font(.system(size: 32))
-                        
-                    }
-                }.onReceive(timer) {
-                    timer in if timeRemaining > 0 {  //TODO: Timer
-                        timeRemaining -= 1
                     }
                 }
                 
-                Spacer()
-                //PAUSE
-                VStack {
-                    Spacer()
+                if breakTime == false {
+                    Text("\(viewModel.timeRemaining)")
+                        .font(.system(size: 32))
                     
-                    if timeRemaining == 0 {
-                        
-                        Button {
-                            //Action
-                        } label: {
-                            Text("Skip break")
-                                .font(.system(size: 18))
-                                .frame(width: 300)
-                                .foregroundColor(Color.black)
-                                .padding()
-                                .overlay(
-                                    RoundedRectangle(cornerRadius: 10)
-                                        .stroke(Color.black, lineWidth: 1))
-                        }.background(Color(Consts.Color.lightGreen))
-                        
-                        Button {
-                            //Action
-                        } label: {
-                            Text("End Session")
-                                .font(.system(size: 18))
-                                .frame(width: 300)
-                                .foregroundColor(Color.black)
-                                .background(Color(Consts.Color.lightRed))
-                                .padding()
-                                .overlay(
-                                    RoundedRectangle(cornerRadius: 10)
-                                        .stroke(Color.black, lineWidth: 1))
-                        }
-                        .background(Color(Consts.Color.lightRed))
-                        Spacer()
-                            .frame(height: 5)
-                        
-                    } else {
-                        
-                        Button {
-                            //Action
-                        } label: {
-                            Text("End Session")
-                                .font(.system(size: 18))
-                                .frame(width: 300)
-                                .foregroundColor(Color.black)
-                                .padding()
-                                .overlay(
-                                    RoundedRectangle(cornerRadius: 10)
-                                        .stroke(Color.black, lineWidth: 1))
-                        }
-                        .background(Color(Consts.Color.lightRed))
-                        Spacer()
-                            .frame(height: 5)
-                    }
+                    
+                } else {
+                    
+                    Text("Pause timer")
+                        .font(.system(size: 32))
+                    
+                    Text("MM:SS") // pause timer logic
+                        .font(.system(size: 32))
+                    
                 }
+            }.onReceive(viewModel.timer) {
+                timer in if viewModel.timeRemaining > 0 {  //TODO: Timer
+                    viewModel.timeRemaining -= 1
+                }
+            }
+            
+            Spacer()
+            //PAUSE
+            VStack {
+                
+                Spacer()
+                
+                if breakTime == true {
+                    
+                    
+                    Button {
+                        //Action
+                    } label: {
+                        Text("Skip break")
+                            .font(.system(size: 18))
+                            .frame(width: 300)
+                            .foregroundColor(Color.black)
+                            .padding()
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(Color.black, lineWidth: 1))
+                    }.background(Color(Consts.Color.lightGreen))
+                    
+                    Button {
+                        //Action
+                    } label: {
+                        Text("End Session")
+                            .font(.system(size: 18))
+                            .frame(width: 300)
+                            .foregroundColor(Color.black)
+                            .background(Color(Consts.Color.lightRed))
+                            .padding()
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(Color.black, lineWidth: 1))
+                    }
+                    .background(Color(Consts.Color.lightRed))
+                    Spacer()
+                        .frame(height: 5)
+                    
+                } else {
+                    
+                    Button {
+                        //Action
+                    } label: {
+                        Text("End Session")
+                            .font(.system(size: 18))
+                            .frame(width: 300)
+                            .foregroundColor(Color.black)
+                            .padding()
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(Color.black, lineWidth: 1))
+                    }
+                    .background(Color(Consts.Color.lightRed))
+                    Spacer()
+                        .frame(height: 5)
+                }
+            }
             .navigationBarTitle(Text("Timer"), displayMode: .inline)
         }
         
         
-        
-        
     }
 }
+
+
 
 
 struct TimerView_Previews: PreviewProvider {
