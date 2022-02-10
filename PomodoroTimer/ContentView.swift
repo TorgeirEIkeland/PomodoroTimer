dce1f4136fbdd20c5e9cb6b6aaccb9b0d5195792
kirizat-theosophe <54266892+kirizat-theosophe@users.noreply.github.com>
2022-02-10 13:07:52 +0100

    ContentView

--- PomodoroTimer/ContentView.swift
+++ PomodoroTimer/ContentView.swift
@@ -8,13 +8,76 @@
 import SwiftUI
 
 struct ContentView: View {
+   
+    @State var title: String = "Pomodoro is a study technique that gives you 25 studytime and 5 minutes rest. Pomodoro is a study technique that gives you 25 studytime and 5 minutes rest.."
+    
     var body: some View {
-        ContentView()
-    }
+        
+            NavigationView{
+                VStack (spacing: 100) {
+                    Text(title)
+                        .font(.title)
+                        .lineLimit(5)
+                        .padding()
+                    
+                    
+                    Spacer()
+                    
+                    NavigationLink( destination: TechniqueView(color: .brown), label: {
+                        Text("Learn more about Pomodoro")
+                            .bold()
+                            .frame(width: 280, height: 50)
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(15)
+                    })
+                    
+                    NavigationLink(destination: MainView(), label: {
+                        Text("Chose topic to study...")
+                            .bold()
+                            .frame(width: 280, height: 50)
+                            .background(Color.green)
+                            .foregroundColor(.white)
+                            .cornerRadius(15)
+                        
+                    })
+                    Spacer()
+                    
+                        .navigationBarItems(
+                            
+                            leading:
+                                HStack {
+                                    Image("redpomo")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: 60, height: 60)
+                                    Image("greenpomo")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: 60, height: 60)
+                                }
+                            ,
+                            trailing:
+                                Image("timepomo")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 60, height: 60)
+                            
+                                .accentColor(.red)
+                            
+                        )
+                }
+            }
+            .accentColor(Color(.label))
+            
+        }
+        
+        
+    
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-} 
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
